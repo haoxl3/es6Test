a80f2d23c8c0ffda13cc9cfe6737c11351907782
haoxl3 <1098262516@qq.com>
2018-02-11 22:14:45 +0800

    jquery of ajax

---
+++ app/js/lottery/interface.js
@@ -0,0 +1,74 @@
+import $ from 'jquery';
+
+class Interface{
+    /**
+     * getOmit 获取遗漏数据(彩票中有好几期不出现的数称为遗漏数据)
+     * @param {string} issue [当前期号]
+     * @return {[type]} [description]
+     */
+    getOmit(issue){
+        let self = this;
+        return new Promise((resolve, reject)=>{
+            $.ajax({
+                url: '/get/omit',
+                data: {
+                    issue: issue
+                },
+                dataType: 'json',
+                success: function(res){
+                    //在其它JS中的方法，通过实例对象来直接调用，避免使用回调，达到数据共享
+                    self.setOmit(res.data);
+                    resolve.call(self.res);
+                },
+                error: function(err){
+                    reject.call(err);
+                }
+            })
+        })
+    }
+    /**
+     * getOpenCode 获取开奖号码
+     */
+    getOpenCode(issue){
+        let self = this;
+        return new Promise((resolve, reject)=>{
+            $.ajax({
+                url: '/get/opencode',
+                data: {
+                    issue: issue 
+                },
+                dataType: 'json',
+                success: function(res){
+                    self.setOpenCode(res.data);
+                    resolve.call(self, res);
+                },
+                error: function(err){
+                    reject.call(err);
+                }
+            })
+        })
+    }
+    /**
+     * getState 获取当前状态
+     */
+    getState(issue){
+        let self = this;
+        return new Promise((resolve, reject)=>{
+            $.ajax({
+                url: '/get/state',
+                data: {
+                    issue: issue 
+                },
+                dataType: 'json',
+                success: function(res){
+                    resolve.call(self, res);
+                },
+                error: function(err){
+                    reject.call(err);
+                }
+            })
+        })
+    }
+}
+
+export default Interface 
